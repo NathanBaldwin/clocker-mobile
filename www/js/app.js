5824b05e9c156205bc263a95e882d2d196090b21
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-03-28 15:06:17 -0500

    updated login controller - now connecting with digital ocean deployed web app

--- www/js/app.js
+++ www/js/app.js
@@ -15,7 +15,7 @@ app
       // for form inputs)
 
       // $rootScope.APIurl = "http://10.0.0.76:3000"
-      $rootScope.APIurl = "http://192.168.1.221:3000"
+      $rootScope.APIurl = "http://clockeronline.com"
 
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
